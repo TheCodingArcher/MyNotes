44bb0048f887173e4ade3c8d90bda9a96614df0b
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-03-06 22:01:27 +0530

    Adding/Deleting Sample Notes

--- app/src/main/java/android/the/coding/archer/mynotes/MainActivity.java
+++ app/src/main/java/android/the/coding/archer/mynotes/MainActivity.java
@@ -1,8 +1,10 @@
 package android.the.coding.archer.mynotes;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -11,9 +13,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -24,8 +29,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        insertNote("New note");
 
         String[] from = { DBOpenHelper.NOTE_TEXT };
         int[] to = { android.R.id.text1 };
@@ -74,5 +77,61 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
         cursorAdapter.swapCursor(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void insertSampleData() {
+        insertNote("Simple Note");
+        insertNote("Multi-line\nNote");
+        insertNote("Simple Note Simple Note Simple Note Simple Note Simple Note Simple Note Simple Note Simple Note Simple Note Simple Note");
+
+        restartLoader();
+    }
+
+    private void deleteAllNotes() {
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            getContentResolver().delete(NotesProvider.CONTENT_URI, null, null);
+
+                            restartLoader();
+
+                            Toast.makeText(MainActivity.this, getString(R.string.all_deleted), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void restartLoader() {
+        getLoaderManager().restartLoader(0, null, this);
     }
 }
