b06b8ec5b31a7fbce5aaa5b263d35514c1bc2316
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-03-07 16:26:12 +0530

    Creating the EditorActivity and adding new notes

---
+++ app/src/main/java/android/the/coding/archer/mynotes/EditorActivity.java
@@ -0,0 +1,32 @@
+package android.the.coding.archer.mynotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
