628c3b88a415add325157d786424c84339f28e2b
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-03-07 15:19:18 +0530

    Customizing Data display with our own CursorAdapter

--- app/src/main/java/android/the/coding/archer/mynotes/MainActivity.java
+++ app/src/main/java/android/the/coding/archer/mynotes/MainActivity.java
@@ -30,17 +30,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] from = { DBOpenHelper.NOTE_TEXT };
-        int[] to = { R.id.tvNote };
-
-        cursorAdapter = new SimpleCursorAdapter(
-                this,
-                R.layout.note_list_item,
-                null,
-                from,
-                to,
-                0
-        );
+        cursorAdapter = new NotesCursorAdapter(this,null,0);
 
         ListView list = findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
